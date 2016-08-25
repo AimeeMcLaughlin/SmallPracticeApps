7e0b50b807045cc0ec5fafd6cfbd4444b338c7ef
AimeeMcLaughlin <aimee_cindy@hotmail.com>
2016-08-25 12:17:17 +0100

    Add Digits Together
    This will split a number into a char array then add each digit together. It will do this until output it just a single digit

---
+++ AddDigits.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int AddDigits(int num) {
+        
+        string myString = num.ToString();
+        
+        var charArray = myString.ToCharArray();
+        
+        int b = myString.Length;
+        
+        int a = 0;
+        
+        if(num > 10)
+        {
+                for(int i = 0; i < b; i++)
+                {
+                    char c = charArray[i];
+                    a = a + int.Parse(c.ToString());
+                }
+                
+                if(a > 10)
+                {
+                    a = AddDigits(a);
+                }
+                else if( a == 10)
+                {
+                    a = a/10;
+                }
+        }
+        else
+        {
+            a = num;
+        }
+        
+        
+        
+        return a;
+    }
+}
